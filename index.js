393d2e3f7d744d4f44f36c52da92b648a5e14c2d
gargpragati9@gmail.com <gargpragati9@gmail.com>
2020-05-13 23:25:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 8000;
+
+
+app.listen(port, function(err){ 
+    if(err){
+        console.log(err);
+    }
+    
+    console.log("server is running")
+});
+

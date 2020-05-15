95921c5f2eda77d8805ee9d18e144efa686ffa82
gargpragati9@gmail.com <gargpragati9@gmail.com>
2020-05-15 20:29:30 +0530

    comments-delete

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,5 @@
-const Post = require('../models/post')
-
+const Post = require('../models/post');
+const Comment = require('../models/comments');
 module.exports.create = function(req,res){
  
      Post.create({
@@ -12,5 +12,22 @@ module.exports.create = function(req,res){
          }
          return res.redirect('back');
      });
+
+}
+
+module.exports.destroy = function(req,res){
+
+   Post.findById(req.params.id ,function(err,post){
+
+       if(post.user == req.user.id){
+           post.remove();
+           Comment.deleteMany({post:req.params.id}, function(err){
+
+            return res.redirect('back');
+           });
+       }else{
+           return res.redirect('back');
+       }
+   })
 
 }

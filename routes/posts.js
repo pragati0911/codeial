29e91bca557391854abbcc5ba474a98ddab8eef9
gargpragati9@gmail.com <gargpragati9@gmail.com>
2020-05-15 19:07:20 +0530

    post-authentication

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
 
 
-router.post('/create', postsController.create);
+router.post('/create', passport.checkAuthentication, postsController.create);
 
 
 module.exports = router;
